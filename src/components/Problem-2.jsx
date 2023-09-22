58eea69b05d47f6c0acd2bd96c966efbf7df7764
Siam Talukder <73644547+siamtalukder484@users.noreply.github.com>
2023-09-23 02:24:23 +0600

    modal added

--- src/components/Problem-2.jsx
+++ src/components/Problem-2.jsx
@@ -1,6 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
+
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 
 const Problem2 = () => {
+
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    const [show2, setShow2] = useState(false);
+    const handleClose2 = () => setShow2(false);
+    const handleShow2 = () => setShow2(true);
 
     return (
 
@@ -9,10 +20,39 @@ const Problem2 = () => {
                 <h4 className='text-center text-uppercase mb-5'>Problem-2</h4>
                 
                 <div className="d-flex justify-content-center gap-3">
-                <button className="btn btn-lg btn-outline-primary" type="button" >All Contacts</button>
-                <button className="btn btn-lg btn-outline-warning" type="button" >US Contacts</button>
+                <button onClick={handleShow} className="btn btn-lg btn-outline-primary" type="button" >All Contacts</button>
+                <button onClick={handleShow2} className="btn btn-lg btn-outline-warning" type="button" >US Contacts</button>
                 </div>
                 
+
+                <Modal show={show} onHide={handleClose}>
+                    <Modal.Header closeButton>
+                    <Modal.Title>Modal A</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
+                    <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+                    </Button>
+                    <Button variant="primary" onClick={handleClose}>
+                        Save Changes
+                    </Button>
+                    </Modal.Footer>
+                </Modal>
+                <Modal show={show2} onHide={handleClose2}>
+                    <Modal.Header closeButton>
+                    <Modal.Title>Modal B</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
+                    <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose2}>
+                        Close
+                    </Button>
+                    <Button variant="primary" onClick={handleClose2}>
+                        Save Changes
+                    </Button>
+                    </Modal.Footer>
+                </Modal>
             </div>
         </div>
     );
